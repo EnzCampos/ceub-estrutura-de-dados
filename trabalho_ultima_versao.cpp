b383e285ad5c49aab9e87d36263939afc039d87a
Enzo Campos <enzocampos@gmail.com>
2023-09-11 13:17:52 -0300

    fix: Sort Counters

--- trabalho_ultima_versao.cpp
+++ trabalho_ultima_versao.cpp
@@ -30,19 +30,6 @@ int menu()
     return op;
 }
 
-void criarArquivo()
-{
-    FILE *arquivoContas = fopen(NOME_ARQUIVO, "w");
-    if (arquivoContas == NULL)
-    {
-        printf("\nNao foi possivel criar o arquivo");
-    }
-    else
-    {
-        fclose(arquivoContas);
-    }
-}
-
 int contaLinhas(FILE *arquivoContas)
 {
     char c;
@@ -69,13 +56,8 @@ void adicionarConta(int maxContas)
     FILE *arquivoContas = fopen(NOME_ARQUIVO, "a+");
     if (arquivoContas == NULL)
     {
-        criarArquivo();
-        arquivoContas = fopen(NOME_ARQUIVO, "a+");
-        if (arquivoContas == NULL)
-        {
-            printf("\nNao foi possivel abrir o arquivo");
-            return;
-        }
+        printf("\nNao foi possivel abrir o arquivo");
+        return;
     }
 
     qtdContas = contaLinhas(arquivoContas) / 3;
@@ -105,7 +87,7 @@ void adicionarConta(int maxContas)
     fclose(arquivoContas);
 }
 
-void lerContas(struct Contas conta[], int *numContas)
+void lerContas(struct Contas conta[], int numContas)
 {
     FILE *arquivoContas = fopen(NOME_ARQUIVO, "r");
 
@@ -115,28 +97,67 @@ void lerContas(struct Contas conta[], int *numContas)
         return;
     }
 
-    int linha = 1;
     int i = 0;
 
     // Cabeçalho da tabela
-    printf("%-10s%-20s%-10s%-10s\n", "Conta", "Nome", "Numero", "Saldo");
+    printf("%-10s%-20s%-10s\n", "Conta", "Nome", "Saldo");
 
     while (fscanf(arquivoContas, "%[^\n]\n%d\n%f\n", conta[i].nome, &conta[i].num, &conta[i].saldo) != EOF)
     {
-        printf("%-10d%-20s%-10d%-10.2f\n", linha, conta[i].nome, conta[i].num, conta[i].saldo);
-
-        linha++;
+        printf("%-10d%-20s%-10.2f\n", conta[i].num, conta[i].nome, conta[i].saldo);
         i++;
-        (*numContas)++;
     }
 
-    if (*numContas <= 0)
+    if (numContas <= 0)
     {
         printf("\nNenhum resultado encontrado no arquivo.\n");
     }
 
-
     fclose(arquivoContas);
+}
+
+void selectionSort(struct Contas conta[], int numContas)
+{
+    printf("\nIniciando Selection Sort\n");
+    clock_t inicio = clock();
+    int i, j, minIndex;
+    struct Contas temp;
+
+    // Para o selection sort, a quantidade de comparações será sempre a mesma, que é o somátorio de 1 até n-1, n sendo o número de contas nesse caso
+    int comparacoes = numContas - 1; // Melhor caso
+    int movimentacoes = 0;
+
+    for (i = 0; i < numContas - 1; i++)
+    {
+        minIndex = i;
+
+        for (j = i + 1; j < numContas; j++)
+        {
+            if (conta[j].num < conta[minIndex].num)
+            {
+                minIndex = j;
+            }
+
+            comparacoes++;
+        }
+
+        if (minIndex != i)
+        {
+            temp = conta[minIndex];
+            conta[minIndex] = conta[i];
+            conta[i] = temp;
+
+            movimentacoes++;
+        }
+    }
+
+    clock_t fim = clock();
+    double tempo_gasto = (double)(fim - inicio) / CLOCKS_PER_SEC;
+
+    printf("\nSelection Sort");
+    printf("\nTempo gasto: %.6f segundos", tempo_gasto);
+    printf("\nComparacoes: %d", comparacoes);
+    printf("\nMovimentacoes: %d\n", movimentacoes);
 }
 
 void insertionSort(struct Contas conta[], int numContas)
@@ -161,61 +182,31 @@ void insertionSort(struct Contas conta[], int numContas)
             j = j - 1;
 
             comparacoes++;
-            movimentacoes++;
         }
 
         conta[j + 1] = chave;
-        movimentacoes++;
-    }
 
-    clock_t fim = clock();
-    double tempo_gasto = (double)(fim - inicio) / CLOCKS_PER_SEC;
-
-    printf("Insertion Sort");
-    printf("\nTempo gasto: %.6f segundos", tempo_gasto);
-    printf("\nComparacoes: %d", comparacoes);
-    printf("\nMovimentacoes: %d\n", movimentacoes);
-}
-
-void selectionSort(struct Contas conta[], int numContas)
-{
-    printf("\nIniciando Selection Sort\n");
-    clock_t inicio = clock();
-    int i, j, minIndex;
-    struct Contas temp;
-
-    int comparacoes = 0;
-    int movimentacoes = 0;
-
-    for (i = 0; i < numContas - 1; i++)
-    {
-        minIndex = i;
-
-        for (j = i + 1; j < numContas; j++)
-        {
-            if (conta[j].num < conta[minIndex].num)
-            {
-                minIndex = j;
-            }
-
-            comparacoes++;
+        if (j + 1 != i) { // Verifica se houve movimentação
+            movimentacoes++;
         }
+    }
 
-        temp = conta[minIndex];
-        conta[minIndex] = conta[i];
-        conta[i] = temp;
-
-        movimentacoes++;
+    // Correção para o melhor caso
+    if (comparacoes == 0 && movimentacoes == 0) {
+        comparacoes = numContas - 1;
     }
 
     clock_t fim = clock();
     double tempo_gasto = (double)(fim - inicio) / CLOCKS_PER_SEC;
 
-    printf("Selection Sort");
+    printf("\nInsertion Sort");
     printf("\nTempo gasto: %.6f segundos", tempo_gasto);
     printf("\nComparacoes: %d", comparacoes);
     printf("\nMovimentacoes: %d\n", movimentacoes);
 }
+
+
+
 
 void ordenarContasArquivo(struct Contas conta[], int numContas, int op)
 {
@@ -235,11 +226,11 @@ void ordenarContasArquivo(struct Contas conta[], int numContas, int op)
 
     if (op == 3)
     {
-        insertionSort(conta, numContas);
+        selectionSort(conta, numContas);
     }
     else if (op == 4)
     {
-        selectionSort(conta, numContas);
+        insertionSort(conta, numContas);
     }
 
     rewind(arquivoContas);
@@ -255,6 +246,7 @@ void ordenarContasArquivo(struct Contas conta[], int numContas, int op)
 void gerarPiorCaso(int numContas)
 {
     FILE *arquivoContas = fopen(NOME_ARQUIVO, "w");
+    srand(time(NULL));
 
     if (arquivoContas == NULL)
     {
@@ -268,7 +260,7 @@ void gerarPiorCaso(int numContas)
         char nomeConta[100];
         sprintf(nomeConta, "Conta %d", i);
 
-        fprintf(arquivoContas, "%s\n%d\n%.2f\n", nomeConta, i, (float)i * 100);
+        fprintf(arquivoContas, "%s\n%d\n%.2f\n", nomeConta, i, (float)(rand() % 1000));
     }
 
     fclose(arquivoContas);
@@ -277,6 +269,8 @@ void gerarPiorCaso(int numContas)
 void gerarMelhorCaso(int numContas)
 {
     FILE *arquivoContas = fopen(NOME_ARQUIVO, "w");
+    srand(time(NULL));
+
 
     if (arquivoContas == NULL)
     {
@@ -290,7 +284,7 @@ void gerarMelhorCaso(int numContas)
         char nomeConta[100];
         sprintf(nomeConta, "Conta %d", i);
 
-        fprintf(arquivoContas, "%s\n%d\n%.2f\n", nomeConta, i * 100, (float)i * 100);
+        fprintf(arquivoContas, "%s\n%d\n%.2f\n", nomeConta, i, (float)(rand() % 1000));
     }
 
     fclose(arquivoContas);
@@ -309,12 +303,29 @@ void gerarCasoMedioAleatorio(int numContas)
     srand(time(NULL));
 
     int i;
-    for (i = 1; i <= numContas; i++)
+    int *numeros = (int *)malloc(numContas * sizeof(int));
+
+    // Preenche o vetor com os números de conta de 1 a numContas
+    for (i = 0; i < numContas; i++)
+    {
+        numeros[i] = i + 1;
+    }
+
+    // Embaralha os números de conta
+    for (i = numContas - 1; i > 0; i--)
+    {
+        int j = rand() % (i + 1);
+        int temp = numeros[i];
+        numeros[i] = numeros[j];
+        numeros[j] = temp;
+    }
+
+    for (i = 0; i < numContas; i++)
     {
         char nomeConta[100];
-        sprintf(nomeConta, "Conta %d", i);
+        sprintf(nomeConta, "Conta %d", numeros[i]);
 
-        fprintf(arquivoContas, "%s\n%d\n%.2f\n", nomeConta, rand() % 1000 + 1, (float)(rand() % 1000));
+        fprintf(arquivoContas, "%s\n%d\n%.2f\n", nomeConta, numeros[i], (float)(rand() % 1000));
     }
 
     fclose(arquivoContas);
@@ -326,9 +337,9 @@ void gerarDados(int numContas)
 
     do
     {
-        printf("\n1) Gerar pior caso.");
-        printf("\n2) Gerar melhor caso.");
-        printf("\n3) Gerar caso medio aleatorio.");
+        printf("\n1) Gerar melhor caso.");
+        printf("\n2) Gerar caso medio aleatorio.");
+        printf("\n3) Gerar pior caso.");
 
         printf("\nEscolha uma operacao: ");
 
@@ -337,15 +348,15 @@ void gerarDados(int numContas)
         switch (option)
         {
         case 1:
-            gerarPiorCaso(numContas);
-            break;
-
-        case 2:
             gerarMelhorCaso(numContas);
             break;
 
-        case 3:
+        case 2:
             gerarCasoMedioAleatorio(numContas);
+            break;
+
+        case 3:
+            gerarPiorCaso(numContas);
             break;
 
         default:
@@ -359,22 +370,61 @@ void gerarDados(int numContas)
 int main()
 {
     int maxContas;
+    int opcaoArquivo;
 
-    do
+    FILE *arquivoContas = fopen(NOME_ARQUIVO, "r");
+    if (arquivoContas != NULL)
     {
-        printf("\nIniciando programa, por favor escolha a quantidade maxima de elementos: ");
+        printf("\nArquivo existente encontrado.\n");
+        printf("1) Continuar usando este arquivo.\n");
+        printf("2) Recriar arquivo do zero.\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcaoArquivo);
 
-        scanf("%d", &maxContas);
-
-        if (maxContas <= 0)
+        if (opcaoArquivo != 1 && opcaoArquivo != 2)
         {
-            printf("\nValor invalido!\n");
+            printf("\nOpcao invalida!\n");
+            fclose(arquivoContas);
             return 0;
         }
 
-    } while (maxContas <= 0);
+        if (opcaoArquivo == 2)
+        {
+            printf("Informe a quantidade maxima de elementos: ");
+            scanf("%d", &maxContas);
+            fclose(arquivoContas);
+            arquivoContas = fopen(NOME_ARQUIVO, "w");
+            fclose(arquivoContas);
+            printf("\nArquivo recriado.\n");
+        }
+        else
+        {
+            maxContas = contaLinhas(arquivoContas) / 3;
+            printf("\nQuantidade maxima de elementos: %d\n", maxContas);
+            fclose(arquivoContas);
+        }
+    }
 
-    struct Contas *conta = (struct Contas *)malloc(maxContas * sizeof(struct Contas)); // Alocagem dinamica de memoria, para o tamanho maximo de contas que o usuario escolheu.
+    else
+    {
+        arquivoContas = fopen(NOME_ARQUIVO, "w");
+        fclose(arquivoContas);
+        printf("\nNovo arquivo criado.\n");
+
+        do
+        {
+            printf("Informe a quantidade maxima de elementos: ");
+            scanf("%d", &maxContas);
+
+            if (maxContas <= 0)
+            {
+                printf("\nValor invalido!\n");
+            }
+
+        } while (maxContas <= 0);
+    }
+
+    struct Contas *conta = (struct Contas *)malloc(maxContas * sizeof(struct Contas)); 
 
     if (conta == NULL)
     {
@@ -385,11 +435,32 @@ int main()
     int numContas = 0;
     int op;
 
-    for (int i = 0; i < maxContas; i++)
+    if (opcaoArquivo == 2)
     {
-        *conta[i].nome = '\0';
-        conta[i].num = 0;
-        conta[i].saldo = 0;
+        for (int i = 0; i < maxContas; i++)
+        {
+            *conta[i].nome = '\0';
+            conta[i].num = 0;
+            conta[i].saldo = 0;
+        }
+    }
+    else
+    {
+        FILE *arquivoContas = fopen(NOME_ARQUIVO, "r");
+        for (int i = 0; i < maxContas; i++)
+        {
+            fscanf(arquivoContas, "%[^\n]\n%d\n%f\n", conta[i].nome, &conta[i].num, &conta[i].saldo);
+
+            if (conta[i].num != 0)
+            {
+                numContas++;
+            }
+
+            if (feof(arquivoContas))
+            {
+                break;
+            }
+        }
     }
 
     do
@@ -403,7 +474,7 @@ int main()
             break;
 
         case 2:
-            lerContas(conta, &numContas);
+            lerContas(conta, numContas);
             break;
 
         case 3:
@@ -416,6 +487,7 @@ int main()
 
         case 5:
             gerarDados(maxContas);
+            numContas = maxContas;
             break;
 
         case 6:
