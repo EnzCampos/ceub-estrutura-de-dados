7c435cdbdf32282f64c34bf857c87a8c97f8d1d8
Enzo Campos <enzocampos@gmail.com>
2023-09-11 10:47:39 -0300

    refactor: Alteração de algumas funcionalidades

--- trabalho_ultima_versao.cpp
+++ trabalho_ultima_versao.cpp
@@ -2,7 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define MAX_CONTAS 5 // Tamanho do array de structs.
+#define MAX_CONTAS 10 // Tamanho do array de structs.
+#define NOME_ARQUIVO "contas.txt" // Nome do arquivo.
 
 struct Contas
 {
@@ -14,21 +15,23 @@ struct Contas
 int menu()
 {
     int op;
-    system("cls"); // O system cleanse retarda a inicializacao do programa, entao quando for testar, comente essa linha.
+
     printf("\n1)Adicionar nova conta");
     printf("\n2)Ler alguma conta");
     printf("\n3)Ordenar as contas (Selection Sort)");
     printf("\n4)Ordenar as contas (Insertion Sort)");
     printf("\n5)Sair");
     printf("\nEscolha uma operacao: ");
+
     scanf("%d", &op);
     getchar();
+
     return op;
 }
 
 void criarArquivo(FILE *CONTAS)
 {
-    CONTAS = fopen("contas.txt", "w");
+    CONTAS = fopen(NOME_ARQUIVO, "w");
     if (CONTAS == NULL)
     {
         printf("\nNao foi possivel criar o arquivo");
@@ -40,23 +43,17 @@ void criarArquivo(FILE *CONTAS)
 // Retorna a quantidade de linhas em um arquivo.
 int contaLinhas(FILE *CONTAS)
 {
-    if (CONTAS == NULL)
-    {
-        printf("\nArquivo nao existe!");
-    }
-    else
-    {
-        char c;
-        int ct = 0;
+    char c;
+    int ct = 0;
 
-        while (c != EOF)
-        {
-            c = fgetc(CONTAS);
-            if (c == '\n')
-                ct = ct + 1;
-        }
-        return ct;
+    while (c != EOF)
+    {
+        c = fgetc(CONTAS);
+        if (c == '\n')
+            ct = ct + 1;
     }
+    
+    return ct;
 }
 
 // Funcao adicionarContas nao armazena as contas adicionadas dentro do struct, apenas dentro do arquivo.
@@ -67,15 +64,16 @@ void adicionarConta(FILE *CONTAS)
     int num;
     float saldo;
 
-    CONTAS = fopen("contas.txt", "a+");
+    CONTAS = fopen(NOME_ARQUIVO, "a+");
 
     if (CONTAS == NULL)
     {
         criarArquivo(CONTAS);
-        CONTAS = fopen("contas.txt", "a+");
+        CONTAS = fopen(NOME_ARQUIVO, "a+");
         if (CONTAS == NULL)
         {
             printf("\nNao foi possivel abrir o arquivo");
+            return;
         }
     }
 
@@ -103,14 +101,15 @@ void adicionarConta(FILE *CONTAS)
         printf("Maximo de contas atingido!\n");
         system("pause");
     }
+    
     fclose(CONTAS);
 }
 
 /* Se o numero de contas em um arquivo ja existente for maior que o valor de MAX_CONTAS
- nao vai haver espaco no struct para armazenar as informacoes. */
+ não vai haver espaco no struct para armazenar as informacoes. */
 void lerContas(FILE *CONTAS, struct Contas conta[])
 {
-    CONTAS = fopen("contas.txt", "r");
+    CONTAS = fopen(NOME_ARQUIVO, "r");
 
     if (CONTAS == NULL)
     {
@@ -118,19 +117,27 @@ void lerContas(FILE *CONTAS, struct Contas conta[])
         return;
     }
 
-    int linha = 1;
+    int linha = 1; 
     int i = 0;
+    int resultados = 0;
 
-    // Modificado na intencao de auxiliar os algoritmos sort.
     while (fscanf(CONTAS, "%[^\n]\n%d\n%f\n", conta[i].nome, &conta[i].num, &conta[i].saldo) != EOF)
     {
         printf("\nConta %d:\n", linha);
         printf("\nNome: %s\n", conta[i].nome);
-        printf("Numero: %d\n", conta[i].num);
-        printf("Saldo: %.2f\n", conta[i].saldo);
+        printf("\nNumero: %d\n", conta[i].num);
+        printf("\nSaldo: %.2f\n", conta[i].saldo);
+
         system("pause");
+
         linha++;
         i++;
+        resultados++;
+    }
+
+    if (resultados <= 0)
+    {
+        printf("\nNenhum resultado encontrado no arquivo.\n");
     }
 
     fclose(CONTAS);
@@ -141,7 +148,7 @@ int main()
     struct Contas conta[MAX_CONTAS];
     FILE *CONTAS;
     CONTAS = NULL;
-    int op, i = 0;
+    int op;
 
     for (int i = 0; i < MAX_CONTAS; i++)
     {
@@ -157,39 +164,29 @@ int main()
         switch (op)
         {
 
-        case 1:
-            adicionarConta(CONTAS);
-            break;
+            case 1:
+                adicionarConta(CONTAS);
+                break;
 
-        case 2:
-            lerContas(CONTAS, conta);
-            break;
+            case 2:
+                lerContas(CONTAS, conta);
+                break;
 
-        case 3:
-            break;
+            case 3:
+                break;
 
-        case 4:
-            break;
+            case 4:
+                break;
 
-        case 5:
-            printf("\nSaindo...\n");
-            break;
+            case 5:
+                printf("\nSaindo...\n");
+                break;
 
-        case 6: // Caso criado para teste. Nao permanecera no codigo final(talvez).
-            for (int i = 0; i < 6; i++)
-            {
-                printf("\n%s", conta[i].nome);
-                printf("\n%d", conta[i].num);
-                printf("\n%f", conta[i].saldo);
-                system("pause");
-            }
-            system("pause");
-            break;
-
-        default:
-            printf("\nOperacao invalida!\n");
-            break;
+            default:
+                printf("\nOperacao invalida!\n");
+                break;
         }
+
     } while (op != 5);
 
     return 0;
